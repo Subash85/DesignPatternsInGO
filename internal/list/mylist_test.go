39a396860bbd472ed547285ad96b5f182175fa2d
Subash.Janakiraman <Subash.Janakiraman@niceincontact.com>
2022-09-20 07:40:57 -0600

    Tests

--- internal/list/mylist_test.go
+++ internal/list/mylist_test.go
@@ -1,1 +1,26 @@
 package list
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSingleList_Add(t *testing.T) {
+	TestsingleList := InitList()
+	name := "First name"
+	TestsingleList.Add(name + "a")
+	TestsingleList.Add(name + "b")
+	assert.NotEmpty(t, TestsingleList, "List should not be empty")
+	assert.Equal(t, 2, TestsingleList.Size(), "The size should be 3")
+}
+
+func TestSingleList_Append(t *testing.T) {
+	TestsingleList := InitList()
+	name := "First name"
+	TestsingleList.Add(name + "a")
+	TestsingleList.Add(name + "b")
+	assert.NotEmpty(t, TestsingleList, "List should not be empty")
+	TestsingleList.Append(name + "c")
+
+	assert.Equal(t, 2, TestsingleList.Size(), "The size should be 3")
+}
